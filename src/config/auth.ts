993b9a3b73bc90a1d9c4057e41b84596d9de8d5e
Artur Ceschin <artur.ceschin@gmail.com>
2022-03-03 23:07:35 -0300

    Authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'f0a38595b870e32bcba46ada35300805',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { AuthenticateUserService } from '../services/AuthenticateUserService';
+
+const sesseionsRouter = Router();
+
+sesseionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+});
+
+export { sesseionsRouter };
