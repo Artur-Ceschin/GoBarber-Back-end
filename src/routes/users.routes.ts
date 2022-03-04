51ef739e5befaa18180e9c4b5d47176858c18849
Artur Ceschin <artur.ceschin@gmail.com>
2022-03-04 11:42:55 -0300

    Image Upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import { CreateUserService } from '../services/CreateUserService';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { UpdateUserAvatarService } from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +29,29 @@ usersRouter.post('/', async (request, response) => {
     }
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateAvatar = new UpdateUserAvatarService();
+
+      const user = await updateAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return response.status(400).json({ error: err.message });
+      }
+    }
+  },
+);
 
 export { usersRouter };
