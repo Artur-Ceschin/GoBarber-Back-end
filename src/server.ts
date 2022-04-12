721e4bcb2b087b02bf6a984bd4fc1b7fd54678cd
Artur-Ceschin <artur.ceschin@gmail.com>
2022-04-11 23:05:36 -0300

    Cors is now enabled in develoment mode

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import routes from './routes';
 import uploadConfig from './config/upload';
-
 import './database';
 import { AppError } from './errors/AppError';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
